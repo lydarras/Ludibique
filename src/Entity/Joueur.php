00e1375de2bc4e82bb12b55c789982e06bd37e5a
lydarras <lydarras@gmail.com>
2023-02-27 16:54:03 +0100

    CSS Form + Gestion erreurs

--- src/Entity/Joueur.php
+++ src/Entity/Joueur.php
@@ -12,7 +12,8 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=JoueurRepository::class)
- * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
+ * @UniqueEntity(fields={"email"}, message="E-mail déjà existant")
+ * @UniqueEntity(fields={"pseudo"}, message="Pseudo déjà existant")
  */
 class Joueur implements UserInterface, PasswordAuthenticatedUserInterface
 {
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,11 +4,11 @@ namespace App\Form;
 
 use App\Entity\Joueur;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -17,20 +17,35 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('pseudo')
+            ->add('email', EmailType::class, [
+                'label' => 'E-mail',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => "Le champ d'e-mail est vide !"
+                    ]),
+                ],
+            ])
+            ->add('pseudo', TextType::class, [
+                'label' => 'Pseudo',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => "Le pseudo est vide !"
+                    ]),
+                ],
+            ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
+                'label' => 'Mot de passe',
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Merci d\'entrer un mot de passe',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Le mot de passe doit avoir au minimum {{ limit }} caractères',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
